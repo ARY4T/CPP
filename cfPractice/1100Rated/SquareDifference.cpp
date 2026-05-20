bd2eb41bede13c202351b006bf3a15bcc7aca4c4
ARY4T <aryathakurx@gmail.com>
2026-05-20 18:27:36 +0530

    1100

---
+++ cfPractice/1100Rated/SquareDifference.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define nl "\n"
+using ll = long long;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int tc{1};cin>>tc;
+    
+    while(tc--){
+        ll a, b;
+        cin>>a>>b;
+
+        ll n1 = a+b;
+        ll n2 = a-b;
+        bool prime = false;
+
+        if(n2 == 1) {
+            int cnt = 0;
+            for(ll i=2; i*i<=n1; ++i){
+                if(n1%i == 0) {
+                    cnt++;
+                    if(n1/i != i) cnt++;
+                }
+                
+                if(cnt > 1) break;
+            }
+            if(cnt == 0) prime = true;
+        }
+
+        if(prime) cout<<"YES"<<nl;
+        else cout<<"NO"<<nl;
+    }      
+  
+}
