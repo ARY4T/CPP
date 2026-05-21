470d0aa4d49b11c359e637d8db01703f0efac193
ARY4T <aryathakurx@gmail.com>
2026-05-21 23:01:58 +0530

    900

---
+++ tle2/m8/primeSubstraction.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define nl "\n"
+using ll = long long;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int tc{1};cin>>tc;
+    
+
+    while(tc--){
+        ll x,y;
+        cin>>x>>y;
+
+        if(x-y > 1) cout<<"YES"<<nl;
+        else cout<<"NO"<<nl;
+    }      
+  
+}
